e86457abdcfeba0018de5af17af3ef6311306671
Tengiz Turdukulov <turdukulov.tengiz@gmail.com>
2024-12-15 18:05:31 +0600

    Add view UserVwContactAgeInDays

---
+++ UsrDevPackageUA/SqlScripts/ContactAgeDaysMSSQL/ContactAgeDaysMSSQL.sql
@@ -0,0 +1,9 @@
+create or alter view UsrVwContactAgeDays
+
+as
+
+select Id as UsrId, Name as UsrName, BirthDate as UsrBirthDate,
+
+datediff(day, BirthDate, getdate()) as UsrAgeDays
+
+from Contact
---
+++ UsrDevPackageUA/SqlScripts/DeleteCatBodyTypeUniversalMSSQL/DeleteCatBodyTypeUniversalMSSQL.sql
@@ -0,0 +1,2 @@
+delete from UsrCarBodyType
+where id = '2d130707-b233-45f7-b049-ec5c429f8dea'
